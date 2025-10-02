3a6ff0f240084f92d629bb57e1981c197ae307af
Arya Soni <aryasoni98@gmail.com>
2025-10-03 00:48:53 +0530

    Fix Homebrew formula: add SHA256 checksum and fix build path

--- Formula/wooak.rb
+++ Formula/wooak.rb
@@ -2,14 +2,14 @@ class Wooak < Formula
   desc "A simple terminal UI for managing SSH connections"
   homepage "https://github.com/aryasoni98/wooak"
   url "https://github.com/aryasoni98/wooak/archive/v0.0.1.tar.gz"
-  sha256 ""
+  sha256 "ccd4708b8783366e23ba940d978be7fd21d8e9cbb9e0bbc92cf6868204b0f639"
   license "MIT"
   head "https://github.com/aryasoni98/wooak.git", branch: "main"
 
   depends_on "go" => :build
 
   def install
-    system "go", "build", *std_go_args(ldflags: "-X main.version=#{version} -X main.gitCommit=#{Utils.git_head}"), "./cmd/main.go"
+    system "go", "build", *std_go_args(ldflags: "-X main.version=#{version}"), "./wooak/cmd/main.go"
   end
 
   test do
