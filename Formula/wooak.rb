ad8c232880fcf8c929c86299e66479d38db64bf1
Arya Soni <aryasoni98@gmail.com>
2025-10-28 22:27:14 +0530

    Bug Fix

--- Formula/wooak.rb
+++ Formula/wooak.rb
@@ -4,7 +4,7 @@ class Wooak < Formula
   url "https://github.com/aryasoni98/wooak/archive/v0.0.1.tar.gz"
   sha256 "ccd4708b8783366e23ba940d978be7fd21d8e9cbb9e0bbc92cf6868204b0f639"
   license "MIT"
-  head "https://github.com/aryasoni98/wooak.git", branch: "main"
+  head "https://github.com/aryasoni98/wooak.git", branch: "master"
 
   depends_on "go" => :build
 
